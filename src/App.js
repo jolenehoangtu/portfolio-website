205b0071e096f9bd76890525d857ecdffc0fd21a
jolenehoangtu <starie1101@gmmail.com>
2019-04-15 13:15:24 +0300

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
-import './App.css';
-
+import Header from './components/Header';
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      resumeData: {}
+    };
+  }
+  /// fetch the resume data and make it new state
+  componentDidMount() {
+    fetch('/resumeData.json')
+      .then(results => {
+        return results.json();
+      })
+      .then(data => {
+        this.setState({ resumeData: data });
+        console.log(this.state.resumeData);
+      });
+  }
   render() {
-    return <div className="App" />;
+    return (
+      <div className="container">
+        <Header data={this.state.resumeData.main} />
+      </div>
+    );
   }
 }
 
